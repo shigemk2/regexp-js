1c2e720dbdfcbc9d10e4ea9c97d12d63c0d8ad14
shigemk2 <i.am.shige@gmail.com>
2014-07-15 10:50:23 +0900

    Apply method chain

--- regexp.js
+++ regexp.js
@@ -1,36 +1,42 @@
-var rgj = {
-  rules: {
-    numericRegex: '^\\d*$',
-    alphaRegex: '^\\D*$'
-  },
-  isNumeric: function(value) {
-    var re = new RegExp(rgj.rules.numericRegex);
-    return value.match(re) !== null;
-  },
-  isAlpha: function(value) {
-    var re = new RegExp(rgj.rules.alphaRegex);
-    return value.match(re) !== null;
-  },
-  isUrl: function(value) {
-    return /^(http|https):\/\//.test(value);
-  }
+function Rpj() {
+  return this.element;
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Rpj.prototype = {
+  _re : '',
+  rules: {
+    startRegex: '^',
+    numericRegex: '\\d*',
+    alphaRegex: '\\D*',
+    urlRegex: '(http|https)',
+    endRegex: '$'
+  },
+  numeric: function() {
+    this._re += Rpj.rules.numericRegex;
+    return this;
+  },
+  alpha: function() {
+    this._re += Rpj.rules.alphaRegex;
+    return this;
+  },
+  url: function() {
+    this._re += Rpj.rules.urlRegex;
+    return this;
+  },
+  start: function() {
+    this._re += Rpj.rules.startRegex;
+    return this;
+  },
+  end: function() {
+    this._re += Rpj.rules.endRegex;
+    return this;
+  },
+  match: function(value) {
+    var re = new RegExp(this._re);
+    this._init();
+    return value.match(re) !== null;
+  },
+  _init: function() {
+    this._re = '';
+  }
+};
