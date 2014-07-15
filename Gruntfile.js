5e9bbcb31a9d3db89764287d0e0affeda7e7cf85
shigemk2 <i.am.shige@gmail.com>
2014-07-15 13:06:39 +0900

    Add grunt-qunit

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,19 +13,23 @@ module.exports = function (grunt) {
                 dest: 'regexp.min.js'
             }
         },
+        qunit: {
+            all: ['test/*.html']
+        },
         watch: {
             gruntfile: {
-                files: '<%= uglify.dist.src %>',
-                tasks: ['uglify']
+                files: ['<%= uglify.dist.src %>','test/*'],
+                tasks: ['uglify', 'qunit']
             }
         }
     });
 
     // These plugins provide necessary tasks
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task
-    grunt.registerTask('default', ['watch', 'uglify']);
+    grunt.registerTask('default', ['watch', 'uglify', 'qunit']);
 };
 
