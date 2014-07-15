886cb40dabf79ba6a240e6da0ad483dfb17ac7ee
shigemk2 <i.am.shige@gmail.com>
2014-07-15 17:07:59 +0900

    Add blankt.js

--- test/regexp-test.js
+++ test/regexp-test.js
@@ -1,13 +1,23 @@
 var rpj;
 
-module("regexp.jsの正常テスト", {
+module("test on regexp.js", {
   setup: function () {
     rpj = new Rpj();
   }
 });
 
-test("check numeric", function () {
-  var rpj = new Rpj();
-  ok(rpj.chkNumeric("1") === true, "1 is valid");
-  ok(rpj.chkNumeric("2") === true, "2 is valid");
+test("numeric on chkNumeric", function () {
+  ok(rpj.chkNumeric("1234567890") === true, "1234567890 is valid");
+});
+
+test("numeric on chkAlpha", function () {
+  ok(rpj.chkAlpha("1234567890") === false, "1234567890 is invalid");
+});
+
+test("alpha on chkAlpha", function () {
+  ok(rpj.chkAlpha("ABCDE") === true, "ABCDE is valid");
+});
+
+test("alpha on chkNumeric", function () {
+  ok(rpj.chkNumeric("ABCDE") === false, "ABCDE is invalid");
 });
