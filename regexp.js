4691d11a2a154307fc417996a8c4952026f39607
shigemk2 <i.am.shige@gmail.com>
2014-07-16 17:23:04 +0900

    Fix code from JSHint

--- regexp.js
+++ regexp.js
@@ -7,82 +7,82 @@
  */
 function Rpj() {
   return this;
-};
+}
 
 Rpj.prototype = {
   rules: {
     numericRegex: '^\\d*$',
     alphaRegex: '^[a-zA-Z]*$',
     alphaNumericRegex: '^[a-zA-Z0-9]*$',
-    alphaExtraRegex: '^[a-zA-Z_\+\-]*$',
-    alphaNumericExtraRegex: '^[a-zA-Z0-9\_\+\-]*$',
+    alphaExtraRegex: '^[a-zA-Z_+-]*$',
+    alphaNumericExtraRegex: '^[a-zA-Z0-9_+-]*$',
     urlRegex: '^(https?:\/\/)'
   },
   // check that value is numeric and check the number is the same as parameter
   chkNumeric: function(value, length) {
     if (! this._chkType(value)) {
       return false;
-    };
+    }
 
     if (typeof length === 'undefined') {
       return this._match(this.rules.numericRegex, value);
     } else {
       return this._match(this.rules.numericRegex, value) && value.length === length;
-    };
+    }
   },
   // check that value is alpha and check the number is the same as parameter
   chkAlpha: function(value, length) {
     if (! this._chkType(value)) {
       return false;
-    };
+    }
 
     if (typeof length === 'undefined') {
       return this._match(this.rules.alphaRegex, value);
     } else {
       return this._match(this.rules.alphaRegex, value) && value.length === length;
-    };
+    }
   },
   // check that value is alpha with _-+ and check the number is the same as parameter
   chkAlphaExtra: function(value, length) {
     if (! this._chkType(value)) {
       return false;
-    };
+    }
 
     if (typeof length === 'undefined') {
       return this._match(this.rules.alphaExtraRegex, value);
     } else {
       return this._match(this.rules.alphaExtraRegex, value) && value.length === length;
-    };
+    }
   },
   // check that value is alphaNumeric and check the number is the same as parameter
   chkAlphaNumeric: function(value, length) {
     if (! this._chkType(value)) {
       return false;
-    };
+    }
 
     if (typeof length === 'undefined') {
       return this._match(this.rules.alphaNumericRegex, value);
     } else {
       return this._match(this.rules.alphaNumericRegex, value) && value.length === length;
-    };
+    }
   },
   // check that value is alphaNumericExtra and check the number is the same as parameter
   chkAlphaNumericExtra: function(value, length) {
     if (! this._chkType(value)) {
       return false;
-    };
+    }
 
     if (typeof length === 'undefined') {
       return this._match(this.rules.alphaNumericExtraRegex, value);
     } else {
       return this._match(this.rules.alphaNumericExtraRegex, value) && value.length === length;
-    };
+    }
   },
   // check that value is url
   chkUrl: function(value) {
     if (! this._chkType(value)) {
       return false;
-    };
+    }
 
     return this._match(this.rules.urlRegex, value);
   },
